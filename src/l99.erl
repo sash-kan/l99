a58028bac978dceca2085533ec7b5633603ecdba
alexander barakin aka sash-kan <alex@barak.in>
2012-04-29 17:34:30 +0400

    p1 revisited

--- src/l99.erl
+++ src/l99.erl
@@ -2,9 +2,8 @@
 -compile(export_all).
 
 % P01 (*) Find the last box of a list.
-p1(L) when is_list(L) -> p1(L,[]).
-p1([],R) -> R;
-p1([H|T],_) -> p1(T,[H]).
+p1(L) when is_list(L), length(L) == 1 -> L;
+p1(L=[_|T]) when length(L) > 1 -> p1(T).
 
 % P02 (*) Find the last but one box of a list.
 p2(L) when is_list(L), length(L) == 2 -> L;
--- src/l99_tests.erl
+++ src/l99_tests.erl
@@ -2,7 +2,7 @@
 -include_lib("eunit/include/eunit.hrl").
 
 p1_test_() -> [
-	 ?_assertEqual([],l99:p1([]))
+	 ?_assertError(function_clause,l99:p1([]))
 	,?_assertEqual([a],l99:p1([a]))
 	,?_assertEqual([b],l99:p1([a,b]))
 	,?_assertEqual([d],l99:p1([a,b,c,d]))
