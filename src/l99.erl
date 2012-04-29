63bc52714ab749fe556c78029253f47c268be48b
alexander barakin aka sash-kan <alex@barak.in>
2012-04-29 17:17:01 +0400

    p1 solution

--- src/l99.erl
+++ src/l99.erl
@@ -1,3 +1,7 @@
 -module(l99).
 -compile(export_all).
 
+% P01 (*) Find the last box of a list.
+p1(L) when is_list(L) -> p1(L,[]).
+p1([],R) -> R;
+p1([H|T],_) -> p1(T,[H]).
--- src/l99_tests.erl
+++ src/l99_tests.erl
@@ -1,3 +1,10 @@
 -module(l99_tests).
 -include_lib("eunit/include/eunit.hrl").
 
+p1_test_() -> [
+	 ?_assertEqual([],l99:p1([]))
+	,?_assertEqual([a],l99:p1([a]))
+	,?_assertEqual([b],l99:p1([a,b]))
+	,?_assertEqual([d],l99:p1([a,b,c,d]))
+	,?_assertError(function_clause,l99:p1(a))
+].
