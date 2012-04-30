4477c69ba37a984d59aacfaa0b45d7f2f4764b7a
alexander barakin aka sash-kan <alex@barak.in>
2012-04-30 21:18:49 +0400

    replace is_number with is_integer

--- src/l99.erl
+++ src/l99.erl
@@ -10,7 +10,7 @@ p2(L) when is_list(L), length(L) == 2 -> L;
 p2(L=[_|T]) when length(L) > 2 -> p2(T).
 
 % P03 (*) Find the K'th element of a list.
-p3(L,N) when is_list(L), is_number(N), N > 0, length(L) >= N -> p3(L,N,1).
+p3(L,N) when is_list(L), is_integer(N), N > 0, length(L) >= N -> p3(L,N,1).
 p3([H|_],N,N) -> H;
 p3([_|T],N,C) -> p3(T,N,C+1).
 
@@ -79,7 +79,7 @@ p11([H|T],R) -> p11(T,[H|R]).
 p12(L) when is_list(L) -> p12(L,[]).
 p12([],R) -> p5(R);
 p12([[0,_]|T],R) -> p12(T,R);
-p12([[N,H]|T],R) when is_number(N), N>0 -> p12([[N-1,H]|T],[H|R]);
+p12([[N,H]|T],R) when is_integer(N), N>0 -> p12([[N-1,H]|T],[H|R]);
 p12([H|T],R) -> p12(T,[H|R]).
 
 % P13 (**) Run-length encoding of a list (direct solution).
@@ -95,19 +95,19 @@ p14([],R) -> p5(R);
 p14([H|T],R) -> p14(T,[H|[H|R]]).
 
 % P15 (**) Replicate the elements of a list a given number of times.
-p15(L,N) when is_list(L), is_number(N), N>0 -> p15(L,N,0,[]).
+p15(L,N) when is_list(L), is_integer(N), N>0 -> p15(L,N,0,[]).
 p15([],_,_,R) -> p5(R);
 p15([_|T],N,N,R) -> p15(T,N,0,R);
 p15(L=[H|_],N,C,R) -> p15(L,N,C+1,[H|R]).
 
 % P16 (**) Drop every N'th element from a list.
-p16(L,N) when is_list(L), is_number(N), length(L) >= N, N>0 -> p16(L,N,1,[]).
+p16(L,N) when is_list(L), is_integer(N), length(L) >= N, N>0 -> p16(L,N,1,[]).
 p16([],_,_,R) -> p5(R);
 p16([_|T],N,N,R) -> p16(T,N,1,R);
 p16([H|T],N,C,R) -> p16(T,N,C+1,[H|R]).
 
 % P17 (*) Split a list into two parts; the length of the first part is given.
-p17(L,N) when is_list(L), is_number(N), length(L) >= N, N>0 ->
+p17(L,N) when is_list(L), is_integer(N), length(L) >= N, N>0 ->
 	p17(L,N,0,[],[]).
 p17([],_,_,L,M) -> [p5(L)]++[p5(M)];
 p17([H|T],N,C,L,[]) when N>C -> p17(T,N,C+1,[H|L],[]);
@@ -115,7 +115,7 @@ p17([H|T],N,C,L,M) -> p17(T,N,C+1,L,[H|M]).
 
 % P18 (**) Extract a slice from a list.
 p18(L,I,K)
-	when is_list(L), is_number(I), is_number(K), I>0, I=<K, length(L)>=K ->
+	when is_list(L), is_integer(I), is_integer(K), I>0, I=<K, length(L)>=K ->
 	p18(L,I,K,1,[]).
 p18([],_,_,_,R) -> p5(R);
 p18(_,_,K,C,R) when C>K -> p18([],0,0,0,R);
@@ -127,38 +127,38 @@ p19(L,N) when N>0 -> [A,B]=p17(L,N), B++A;
 p19(L,N) when N<0 -> [A,B]=p17(L,length(L)+N), B++A.
 
 % P20 (*) Remove the K'th element from a list.
-p20(L,K) when is_list(L), is_number(K), K>0, length(L)>=K -> p20(L,K,1,[]).
+p20(L,K) when is_list(L), is_integer(K), K>0, length(L)>=K -> p20(L,K,1,[]).
 p20([],_,_,R) -> p5(R);
 p20([_|T],K,K,R) -> p20(T,K,K+1,R);
 p20([H|T],K,C,R) -> p20(T,K,C+1,[H|R]).
 
 % P21 (*) Insert an element at a given position into a list.
-p21(E,L,N) when is_list(L), is_number(N), N>0, length(L)>=N-1 ->
+p21(E,L,N) when is_list(L), is_integer(N), N>0, length(L)>=N-1 ->
 	p21(E,L,N,1,[]).
 p21(E,L,N,N,R) -> p21(E,L,N,N+1,[E|R]);
 p21(_,[],_,_,R) -> p5(R);
 p21(E,[H|T],N,C,R) -> p21(E,T,N,C+1,[H|R]).
 
 % P22 (*) Create a list containing all integers within a given range.
-p22(I,K) when is_number(I), is_number(K), I=<K -> p22(I,K,[]).
+p22(I,K) when is_integer(I), is_integer(K), I=<K -> p22(I,K,[]).
 p22(N,N,R) -> p5([N|R]);
 p22(I,K,R) -> p22(I+1,K,[I|R]).
 
 % P23 (**) Extract a given number of randomly selected elements from a list.
-p23(L,N) when is_list(L), is_number(N), N>=0, length(L)>=N -> 
+p23(L,N) when is_list(L), is_integer(N), N>=0, length(L)>=N -> 
 	random:seed(now()), p23(L,N,[]).
 p23(_,0,R) -> R;
 p23(L,N,R) -> K=random:uniform(length(L)), p23(p20(L,K),N-1,[p3(L,K)|R]).
 
 % P24 (*) Lotto: Draw N different random numbers from the set 1..M.
-p24(N,M) when is_number(N), is_number(M), N>0, M>=N -> p23(p22(1,M),N).
+p24(N,M) when is_integer(N), is_integer(M), N>0, M>=N -> p23(p22(1,M),N).
 
 % P25 (*) Generate a random permutation of the elements of a list.
 p25(L) when is_list(L) -> p23(L,length(L)).
 
 % P26 (**) Generate the combinations of K distinct objects chosen from the N elements of a list
 p26(1,L) when is_list(L) -> [[X]||X <- L];
-p26(N,L) when is_list(L), is_number(N), N>1 ->
+p26(N,L) when is_list(L), is_integer(N), N>1 ->
 	[ [X|Y] || X <- L, Y <- p26(N-1,p26(p5(L),[X],[])) ].
 p26([],_,R) -> R;
 p26([X|_],[X],R) -> R;
@@ -196,7 +196,7 @@ p28_m([[FH|H]|T],F,R) when F>=FH -> p28_m(T,F,[[F|H]|R]);
 p28_m([[FH|H]|T],_,R) -> p28_m(T,FH,[[FH|H]|R]).
 
 % P31 (**) Determine whether a given integer number is prime.
-p31(N) when is_number(N), N>1 -> p31(N,2).
+p31(N) when is_integer(N), N>1 -> p31(N,2).
 p31(N,K) when K*K>N -> true;
 p31(N,K) when N==trunc(N/K)*K -> false;
 p31(N,K) -> p31(N,K+1).
